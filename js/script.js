55f34f0af3297a1d68577076ea294c12faa0bf23
525gtarantola <525gtarantola@frhsd.com>
2024-09-30 08:49:10 -0400

    class tutorial done

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-$("document").ready(function () {
-  $(".swatch").on("click", function () {
+$(document).ready(function () {
+  $(".swatch").on("mouseenter", function () {
     $("#product-photo").attr(
       "src",
       "img/chair-" + $(this).attr("id") + "@2x.jpg"
     );
+    $(".swatch").removeClass("selected");
+    $(this).addClass("selected");
   });
 });
